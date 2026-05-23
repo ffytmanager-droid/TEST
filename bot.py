3c04e1508b404a40e75a2ed9b18955474b68db51
ffytmanager-droid <ffytmanager@gmail.com>
2026-05-24 03:03:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,22 @@
 
+import os
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+load_dotenv()
+
+TOKEN = os.getenv('BOT_TOKEN')
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Bot is running! ✅')
+
+if __name__ == '__main__':
+    if not TOKEN:
+        print("Error: Token Not Found. Check .env again!")
+    else:
+        app = ApplicationBuilder().token(TOKEN).build()
+        app.add_handler(CommandHandler("start", start))
+        
+        print("Bot Started!")
+        app.run_polling()
